507fdafe47500c5f62f9691c9520bcb06d22cad9
Thorbl <game2world@yahoo.ca>
2016-12-10 21:41:37 -0500

    Creation of all files and settings

---
+++ .Template.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+using VRageMath;
+using VRage.Game;
+using VRage.Collections;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using Sandbox.Game.EntityComponents;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+
+namespace SpaceEngineers
+{
+    public sealed class Program : MyGridProgram
+    {
+        //=======================================================================
+        //////////////////////////BEGIN//////////////////////////////////////////
+        //=======================================================================
+
+        public Program()
+        {
+
+        }
+
+        public void Main(string args)
+        {
+
+        }
+
+        public void Save()
+        {
+
+        }
+
+        //=======================================================================
+        //////////////////////////END////////////////////////////////////////////
+        //=======================================================================
+    }
+}
